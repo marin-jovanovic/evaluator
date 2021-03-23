9d0d3a77319c2b53afb1da10c139c40c0da01d84
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-03-23 12:56:11 +0100

    :recycle: code cleanup

--- setup.py
+++ setup.py
@@ -3,16 +3,12 @@ from tkinter import filedialog
 
 
 def append_to_log(data):
-    log = open("log.txt", "a")
+    log = open("log.txt", "w")
     log.write(data + "\n")
     log.close()
 
-def get_log():
-    for line in open("log.txt").readlines():
-        print(line)
 
 def file_chooser():
-    # print(message)
     root = tkinter.Tk()
     root.withdraw()
 
@@ -32,6 +28,7 @@ def dir_chooser():
 PROGRAM_PATH = ""
 DIR_PATH = ""
 
+
 def check_driver():
     from os import path
     global check
@@ -47,21 +44,12 @@ def check_driver():
     elif path.exists(DIR_PATH):
         check.delete(0, tkinter.END)
         check.insert(0, "no program")
-        # check.insert(1, "no dir path")
-        # print("Error occured during opening program\nCheck \"PROGRAM_PATH\" constant")
-        # import sys
-        # sys.exit()
-    # if file(PROGRAM_PATH).
-    # global entry1, entry2
-
 
 
 def file_driver():
     global PROGRAM_PATH
     temp = file_chooser()
     PROGRAM_PATH = temp
-    # append_to_log(temp)
-    # append_to_log(file_chooser())
     global entry1
     entry1.delete(0, tkinter.END)
     entry1.insert(0, temp)
@@ -74,79 +62,54 @@ def dir_driver():
 
     temp = dir_chooser()
     DIR_PATH = temp
-    # append_to_log(dir_chooser())
+
     global entry2
     entry2.delete(0, tkinter.END)
     entry2.insert(0, temp)
+
     check_driver()
+
 
 def on_closing():
     global root
     import tkinter.messagebox
     if tkinter.messagebox.askokcancel("Quit", "Do you want to quit?"):
         print("saving")
-        with open("log.txt", "r+") as f:
-            data = f.read()
-            f.seek(0)
-            f.write(PROGRAM_PATH + "\n" + DIR_PATH + "\n")
-            f.truncate()
+
+        append_to_log(PROGRAM_PATH + "\n" + DIR_PATH)
 
         root.destroy()
 
 
-
-
 if __name__ == '__main__':
-    # global root, entry1, entry2
-    # global canvas1
     print("welcome to setup script")
     print("if you do not see tkinter window go to log.txt")
     print("1. row = programs absolute path")
-print("2. row = directory with tests")
+    print("2. row = directory with tests")
     print("3. row = empty")
 
     root = tkinter.Tk()
-
-    # root = tkinter.Tk()
-    left_offset = 10
 
     canvas1 = tkinter.Canvas(root, width=1000, height=1000)
     canvas1.pack()
 
     entry1 = tkinter.Entry(root)
-    entry1.place(x=left_offset,
-                       y=10,
-                       width=500,
-                       height=100)
+    entry1.place(x=10, y=10, width=500, height=100)
 
     entry2 = tkinter.Entry(root)
-    entry2.place(x=left_offset,
-                       y=300,
-                       width=500,
-                       height=100)
+    entry2.place(x=10, y=300, width=500, height=100)
 
     check = tkinter.Entry(root)
-    check.place(x=left_offset,
-                       y=600,
-                       width=500,
-                       height=100)
-
-    # entry2 = tkinter.Entry(root)
-
-    # entry1 = tkinter.Entry(root)
+    check.place(x=10, y=600, width=500, height=100)
 
     button1 = tkinter.Button(text='select path to program', command=file_driver)
     print(entry1.index(tkinter.INSERT))
-    canvas1.create_window(left_offset + 100, 150, window=button1)
-
-    # canvas1.create_window(100, 200, window=entry2)
+    canvas1.create_window(110, 150, window=button1)
 
     button2 = tkinter.Button(text='select dir to tests', command=dir_driver)
-    canvas1.create_window(left_offset + 100, 250, window=button2)
+    canvas1.create_window(110, 250, window=button2)
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
 
     root.mainloop()
     print("setup done, now start main")
-
-
